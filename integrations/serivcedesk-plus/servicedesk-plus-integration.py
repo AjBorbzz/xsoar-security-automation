6403efc8c2bb572b4404af0d7ce3d4e6c981bed8
AjBorbzz <ajborbeheaddhz@yahoo.com>
2026-05-14 00:15:39 +0800

    Added get request conversation method

--- integrations/serivcedesk-plus/servicedesk-plus-integration.py
+++ integrations/serivcedesk-plus/servicedesk-plus-integration.py
@@ -19,4 +19,28 @@ class Client(BaseClient):
     # updated http_request : added data parameter
     def http_request(self, method, url_suffix, full_url=None, params=None, data=None):
         ok_codes = (200, 201, 401) # error response was included -> XSOAR OOTB 
+        # update the self._http_request -> from BaseClient of XSOAR integration : add data parameter
+        try:
+            res = self._http_request(method, url_suffix, full_url=full_url, resp_type='response', ok_codes=ok_codes,
+                                     params=params, data=data)
+            # Other script goes here #
+        except Exception as e:
+            if 'SSL Certificate Verification Failed' in e.args[0]:
+                return_error('SSL Certificate Verification Failed - try selecting \'Trust any certificate\' '
+                             'checkbox in the integration configuration.')
+            raise DemistoException(e.args[0])
+
+    def get_requests_conversations(self, request_id: str = None, params: dict = None):
+        """Get All conversations under requests.
         
+            request_id : 18 digit id of the Request. 
+            params: payload of the Request.
+        """
+        if request_id:
+            try:
+                res = self.http_request(method='GET', url_suffix=f'requests/{request_id}/conversations', params=params)
+                return res 
+            except Exception as err:
+                notif_unavailable = f'{err}'
+                if '"status_code: 4000"' in notif_unavailable or '"status_code": 4007' in notif_unavailable:
+                    return None
